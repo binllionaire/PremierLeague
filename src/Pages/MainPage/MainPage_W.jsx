36bd754e096f261d328a750da74d0e18dec1ec93
binllionaire <bug20474@gmail.com>
2022-12-31 17:37:36 +0900

    feat : 스크롤 시 색상 변경

--- src/Pages/MainPage/MainPage_W.jsx
+++ src/Pages/MainPage/MainPage_W.jsx
@@ -1,8 +1,25 @@
 import React, { useEffect, useState } from "react";
 
 const MainPage_W = () => {
+    useEffect(() => {
+        
+        window.addEventListener('scroll', handleScroll, { capture: true }); // 스크롤 이벤트 등록
+        return () => {
+          window.removeEventListener('scroll', handleScroll); 		// 스크롤 이벤트 제거
+        };
+      }, []);
+    const [isScrolled, setIsScrolled] = useState(false);
+    const handleScroll = () => {
+        console.log(window.scrollY)
+        if(window.scrollY >= 150){
+            setIsScrolled(true);
+        }
+        else{
+            setIsScrolled(false);
+        }
+    }
     return(
-    <div className = "main-page">
+    <div className = {isScrolled ? "main-page-scroll" :"main-page"}>
         <div className="nav-section">
             <nav id="nav-wrapper">
                     <img id="open" src="/img/plus.png" width="50px" height="50px"></img>
@@ -40,6 +57,20 @@ const MainPage_W = () => {
         .main-page{
             width:100%;
             background-color: #d91a32;
+            transition-duration: 0.5s;
+            transition-timing-function: ease;
+            -webkit-transition-duration:0.4s;
+            -webkit-transition-timing-function:ease;
+        }
+        .main-page-scroll{
+            width:100%;
+            margin: 0 auto 0 auto;
+            color:white;
+            background-color: black;
+            transition-duration: 0.5s;
+            transition-timing-function: ease;
+            -webkit-transition-duration:0.4s;
+            -webkit-transition-timing-function:ease;
         }
         .nav-section{
             width:85%;
@@ -53,7 +84,6 @@ const MainPage_W = () => {
             cursor:pointer;
         }
         .header-section{
-            background-color: #d91a32;
             margin: 0 auto 0 auto;
             width:85%;
         }
@@ -61,6 +91,7 @@ const MainPage_W = () => {
             border-top:2px solid black;
             height:40px;
             margin : 0 auto 20px auto;
+            animation: divider 1s ease-in-out both;
         }
         h1{
             font-family: 'Archivo';
@@ -71,16 +102,18 @@ const MainPage_W = () => {
             margin-bottom: 0px;
             position: relative;
             display: block;
+            animation: h1-anim 1s ease-in-out both;
         }
         #open{
             transition: all ease 1s;
+            animation: fadeInDown 1s;
             
         }
         #open:hover {
             transform: rotate(135deg );
             transform-origin : 50% 50%;
         }
-        @keyframes .header-intro {
+        @keyframes h1-anim {
             0%{
             clip-path:polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%);
             transform:translateY(100%);
@@ -92,6 +125,16 @@ const MainPage_W = () => {
                 clip-path:polygon(0% 00%, 100% 0%, 100% 100%, 0% 100%);
             }
         }
+        @keyframes fadeInDown {
+        0% {
+            opacity: 0;
+            transform: translate3d(0, -100%, 0);
+        }
+        to {
+            opacity: 1;
+            transform: translateZ(0);
+        }
+    }
         `}</style>
     </div>
     );
